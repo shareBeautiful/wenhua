09176784e706594a6a9f1e0c6ca25349c5ccb0d4
HOME\zhouz <dev>
2022-01-19 18:00:43 +0800

    foxue

--- js/content.js
+++ js/content.js
@@ -154,7 +154,7 @@ function getContent() {
                     allTotal: 0, // 总字数
                     font: [20, 22, 24, 26, 28],
                     fontF: 
-                    [{n:'默认',v:'inherit'},{n:'汉仪',v:'hanyi'},{n:'cc',v:'cc'},{n:'仓耳', v:'canger'},{n:'今楷', v:'jinkai'},{n:'行黑',v:'xinghei'},{n:'卡通',v:'katong'}],
+                    [{n:'默认',v:'inherit'},{n:'汉仪',v:'hanyi'},{n:'仓耳', v:'canger'},{n:'今楷', v:'jinkai'},{n:'行黑',v:'xinghei'},{n:'卡通',v:'katong'}],
                     currFont: 22,
                     currFontF: 'inherit',
                     // footMenu: [{n:'目录'},{n:'进度'},{n:'设置'},{n:'夜间'}],
