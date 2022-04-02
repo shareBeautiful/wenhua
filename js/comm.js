53b3167abbdd602eb2906670af86944303d70275
HOME\zhouz <dev>
2022-04-02 13:00:04 +0800

    foxue

--- js/comm.js
+++ js/comm.js
@@ -6,10 +6,7 @@
     // 定义通用变量
     function Echo() {
         this.VERSION = '0.02';
-        this.apiUrl = "http://192.168.0.27:8080/lingjuyi"; // 兰工本地
-        // this.apiUrl = "https://isv.first-zone.cn/llt_api/lingjuyi";  //正式环境 1
-        // this.apiUrl = "https://isv.first-zone.cn/mall_api/lingjuyi";  //正式环境 2
-        // this.imgUrl = "https://isv.first-zone.cn/mall_api/lingjuyi/file";
+        this.apiUrl = "";
     }
 
     // 定义通用方法
