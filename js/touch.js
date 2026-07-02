bc7d9e748635537bdb399389966d672692ec2140
honor <1454167941@qq.com>
2026-07-02 13:12:56 +0800

    wenhua

--- js/touch.js
+++ js/touch.js
@@ -1,154 +1,85 @@
-function VueTouch(el, binding, type) {
-    var g = this;
-
-    g.obj = el;
-    g.binding = binding;
-    g.touchType = type;
-
-    g.firstTouchPosition = {
-        x: 0,
-        y: 0
-    };
-    g.firstTouchTime = 0;
-    g.callBack = typeof (binding.value) === "object" ? binding.value.fn : binding.value;
-
-    // 可选配置：支持通过 binding.value.options 传入自定义阈值
-    g.options = typeof (binding.value) === "object" ? (binding.value.options || {}) : {};
-    g.swipeDistance = g.options.swipeDistance || 20; // 最小滑动距离(px)
-    g.swipeTime = g.options.swipeTime || 300; // 最大滑动时间(ms)
-    g.angleTolerance = g.options.angleTolerance || 20; // 角度容差(度)
-
-    g.moved = false;
-    g.leaved = false;
-    g.longTouched = false;
-
-    // 事件监听回调集合
-    var _listener = Object.create(null);
-
-    // 事件方法
-    var _listen = (type) => {
-        return function (e) {
-            var {
-                stop,
-                prevent,
-                self,
-                once
-            } = g.binding.modifiers;
-
-            // 配置判断
-            if (stop) e.stopPropagation();
-            if (prevent) e.preventDefault();
-            if (once) g.obj.removeEventListener("touch" + type, _listener[type]);
-            if (self && e.target !== e.currentTarget) return;
-
-            g[type](e);
-        }
-    };
-
-    _listener.start = _listen('start');
-    _listener.end = _listen('end');
-    _listener.move = _listen('move');
-
-    this.obj.addEventListener("touchstart", _listener.start, false);
-    this.obj.addEventListener("touchend", _listener.end, false);
-    this.obj.addEventListener("touchmove", _listener.move, false);
+function vueTouch(el, binding, type, vnode) {
+    this.obj = el;
+    this.binding = binding;
+    this.touchType = type;
+    this.vueTouches = { x: 0, y: 0 };
+    this.vueMoves = true;
+    this.vueLeave = true;
+    this.longTouch = true;
+    this.vueCallBack = binding.value;
+    this.obj.addEventListener("touchstart", (e) => {
+        this.start(e);
+    }, false);
+    this.obj.addEventListener("touchend", (e) => {
+        this.end(e);
+    }, false);
+    this.obj.addEventListener("touchmove", (e) => {
+        this.move(e);
+    }, false);
     // vnode.key = this.randomString()
 
 };
-VueTouch.prototype = {
+vueTouch.prototype = {
     start: function (e) {
-        this.d = 20
-        var g = this;
-
-        // 初始化点击状态
-        g.moved = false;
-        g.leaved = false;
-        g.longTouched = false;
-
-        g.firstTouchPosition = g.getMultiCenter(e.changedTouches);
-        g.firstTouchTime = e.timeStamp;
-        g.time = setTimeout(function () {
-            if (!g.leaved && !g.moved) {
-                g.touchType === "longtap" && g.callBack(e, g.binding.value);
-                g.longTouched = true;
-            }
-        }.bind(g), 1000);
+        this.vueMoves = true;
+        this.vueLeave = true;
+        this.longTouch = true;
+        this.vueTouches = { x: e.changedTouches[0].pageX, y: e.changedTouches[0].pageY };
+        this.time = setTimeout(function () {
+            if (this.vueLeave && this.vueMoves) {
+                this.touchType == "longtap" && this.vueCallBack();
+                this.longTouch = false;
+            };
+        }.bind(this), 1000);
     },
     end: function (e) {
-        var g = this;
-
-        var {
-            x,
-            y
-        } = g.getMultiCenter(e.changedTouches);
-        var _disX = x - g.firstTouchPosition.x;
-        var _disY = y - g.firstTouchPosition.y;
-        var _dis = Math.sqrt(_disX * _disX + _disY * _disY);
-        var _timeDis = e.timeStamp - g.firstTouchTime;
-
-        clearTimeout(g.time);
-
-        var _angle = this.getAngle(_disX, _disY);
-
-        if (_dis > g.swipeDistance && _timeDis < g.swipeTime) {
-            g.touchType === "swipe" && g.callBack(e, g.binding.value);
-            var tol = g.angleTolerance;
-            // 更稳健的角度判断：基于目标角度与容差
-            if (Math.abs(_angle - 90) <= tol)
-                g.touchType === "swipedown" && g.callBack(e, g.binding.value);
-            if (Math.abs(_angle + 90) <= tol)
-                g.touchType === "swipeup" && g.callBack(e, g.binding.value);
-            if (Math.abs(_angle) <= tol)
-                g.touchType === "swiperight" && g.callBack(e, g.binding.value);
-            if (Math.abs(Math.abs(_angle) - 180) <= tol)
-                g.touchType === "swipeleft" && g.callBack(e, g.binding.value);
+        var disX = e.changedTouches[0].pageX - this.vueTouches.x;
+        var disY = e.changedTouches[0].pageY - this.vueTouches.y;
+        clearTimeout(this.time);
+        if (Math.abs(disX) > 10 || Math.abs(disY) > 100) {
+            this.touchType == "swipe" && this.vueCallBack(e);
+            if (Math.abs(disX) > Math.abs(disY)) {
+                if (disX > 10) {
+                    this.touchType == "swiperight" && this.vueCallBack(e);
+                };
+                if (disX < -10) {
+                    this.touchType == "swipeleft" && this.vueCallBack(e);
+                };
+            } else {
+                if (disY > 10) {
+                    this.touchType == "swipedown" && this.vueCallBack(e);
+                };
+                if (disY < -10) {
+                    this.touchType == "swipeup" && this.vueCallBack(e);
+                };
+            };
         } else {
-            if (!g.longTouched && !g.moved) {
-                g.touchType === "tap" && g.callBack(e, g.binding.value);
-                g.leaved = true;
-            }
-        }
-    },
-    move: function (e) {
-        this.moved = true;
-    },
-
-    /**
-     * 获取点击集合的中心坐标
-     * @param touches touch对象集合
-     * @return {{x: number, y: number}}
-     */
-    getMultiCenter: function (touches) {
-
-        var g = this,
-            x = 0,
-            y = 0;
-
-        const _length = touches.length;
-
-        for (var i = 0; i < _length; i++) {
-            x += touches[i].pageX;
-            y += touches[i].pageY;
-        }
-
-        return {
-            x: Math.round(x / _length),
-            y: Math.round(y / _length)
+            if (this.longTouch && this.vueMoves) {
+                this.touchType == "tap" && this.vueCallBack(e);
+                this.vueLeave = false
+            };
         };
     },
-
-    getAngle: function (disX, disY) {
-        var g = this;
-        if (typeof disX !== 'number' || typeof disY !== 'number')
-            return 45;
-        return Math.atan2(disY, disX) * 180 / Math.PI;
+    move: function (e) {
+        this.vueMoves = false;
+    },
+    randomString: function () {
+        var len = 10;
+        var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+        var maxPos = $chars.length;
+        var pwd = '';
+        for (var i = 0; i < len; i++) {
+            pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+        }
+        return pwd;
     }
 };
+
 var eArr = ['tap', 'swipe', 'swipeleft', 'swiperight', 'swipedown', 'swipeup', 'longtap'];
-eArr.forEach(function(s) {
+eArr.forEach(function (s) {
     Vue.directive(s, {
         bind: function (el, binding) {
-            new VueTouch(el, binding, s);
+            new vueTouch(el, binding, s);
         }
     });
 });
